4add3875e5edb2e9a54faecf4b97e4b135022f31
Durga9705 <145944234+Durga9705@users.noreply.github.com>
2026-08-16 15:30:14 +0530

    Refactor Firebase setup in app.js
    Updated Firebase imports and initialized app with Firestore and Auth.

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
- import firebaseConfig from "./config.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import firebaseConfig from "./config.js";
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export const MASTER_ADMIN_EMAIL = "durgamanjula9705@gmail.com";
+export const MASTER_ADMIN_PASS = "DURGA@8405";
